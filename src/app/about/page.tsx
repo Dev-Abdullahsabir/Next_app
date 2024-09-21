c7a0081d0cdf369307156b6080b58f4e2437469e
Dev-Abdullahsabir <dev.abdullahsabir@gmail.com>
2024-09-21 19:46:32 +0500

    Second commit

---
+++ src/app/about/page.tsx
@@ -0,0 +1,8 @@
+export default function About() {
+    return (
+      <>
+        <h1>Hello from Abdullah In About</h1>
+        <h1>Next.js</h1>
+      </>
+    );
+  }
---
+++ src/app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <>
+      <h1>Hello from Abdullah</h1>
+      <h1>Next.js</h1>
+    </>
+  );
+}
